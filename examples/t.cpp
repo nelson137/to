8e12866e37826ddcfb78ddb2806769dbf2075eb3
Nelson Earle <Nelson.Earle@vu.com>
2020-06-25 12:09:23 -0500

    fix printing cli args

--- examples/t.cpp
+++ examples/t.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     cout << "hello from c++" << endl;
-    for (int i=0; i<0; ++i)
+    for (int i = 0; i < argc; ++i)
         cout << "  " << i << " : " << argv[i] << endl;
     return 0;
 }
